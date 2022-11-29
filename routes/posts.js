6b4d5480b6df50514c79d4d013674afa8114c2d6
raihanmahmudiut <raihanmahmudiut@gmail.com>
2022-11-30 02:10:38 +0600

    Pushed the main files

---
+++ routes/posts.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const verify = require('./verifyToken')
+
+
+router.get('/', verify, (req, res) => {
+    res.send(req.user);
+    User.findOne({_id: req.user})
+    });
+
+
+module.exports = router;
